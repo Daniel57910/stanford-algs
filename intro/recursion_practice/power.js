e8a43d6964aed9e5f40e190562c5de91c7477435
Daniel Miller <danielmiller5791@gmail.com>
2018-08-27 16:22:57 +0100

    testing infrastructure setup

---
+++ intro/recursion_practice/power.js
@@ -0,0 +1,5 @@
+class Power {
+
+}
+
+module.exports = Power
---
+++ intro/recursion_practice/power.spec.js
@@ -0,0 +1,7 @@
+const testPower = require ('./power')
+
+describe('Test Power', () => {
+  it('returns the number of times an integer can be expressed in unique, natural numbers', () => {
+    expect(2).toEqual(2)
+  })
+})

a927b9e3698deb4c9f796a706e589e5f53f4ad3f
Daniel Miller <danielmiller5791@gmail.com>
2018-08-28 11:33:27 +0100

    logic extracted from main function

--- intro/recursion_practice/digit.js
+++ intro/recursion_practice/digit.js
@@ -1,13 +1,15 @@
 class Digit {
 
   digitSum(n) {
-    return (n.toString().split('').map(x => Number(x)).reduce((a, b) => a + b))
+    return addTheDigits(0, n)
   }
 
+}
+
+function addTheDigits(x = 0, n) {
+  return (n.toString().split('').map(x => Number(x)).reduce((a, b) => a + b))
 }
 
 module.exports = Digit
 
 
-let a = 9999
-console.log(a.toString().split('').map(x => Number(x)).reduce((a, b) => a + b))

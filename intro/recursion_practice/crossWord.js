8e83984da5b6119a27acbc295eab2e8a01084eec
Daniel Miller <danielmiller5791@gmail.com>
2018-08-28 16:42:01 +0100

    small bug fixed

--- intro/recursion_practice/crossWord.js
+++ intro/recursion_practice/crossWord.js
@@ -22,12 +22,20 @@ function solve(y, x, crossword, word) {
     crossword[y][x] = word[0]
     word = word.slice(1, word.length)
   }
+  
+  if (x >= 3) {
+    return solve(y + 1, 0, crossword, word)
+  }
 
-  if (x < 2) {
+  if (y >= 3) {
+    return solve(0, x + 1, crossword, word )
+  }
+  
+  if (x < 3) {
     return solve(y, x + 1, crossword, word)
   }
 
-  if (y < 2) {
+  if (y < 3) {
     return solve(y + 1, x , crossword, word)
   }
 
--- intro/recursion_practice/crossWord.spec.js
+++ intro/recursion_practice/crossWord.spec.js
@@ -3,16 +3,22 @@ const wordGame = require('./crossWord')
 describe('basic crossword with one word', () => {
   beforeEach(() => {
     let crossWord = []
-    crossWord[0] = new Array('X', 'X', '-')
-    crossWord[1] = new Array('X', 'X', '-')
-    crossWord[2] = new Array('X', 'X', '-')
+    crossWord[0] = new Array('X', 'X', '-', 'X')
+    crossWord[1] = new Array('X', 'X', '-', 'X')
+    crossWord[2] = new Array('X', 'X', '-', 'X')
     let word = ['dog']
     game = new wordGame()
     game.crosswordPuzzle(crossWord, word)
   })
   it('generates a crossWord to be recursively filled out', () => {
-    expect(game.crossword[0]).toEqual(['X', 'X', 'd'])
-    expect(game.crossword[1]).toEqual(['X', 'X', 'o'])
-    expect(game.crossword[2]).toEqual(['X', 'X', 'g'])
+    expect(game.crossword[0]).toEqual(['X', 'X', 'd', 'X'])
+    expect(game.crossword[1]).toEqual(['X', 'X', 'o', 'X'])
+    expect(game.crossword[2]).toEqual(['X', 'X', 'g', 'X'])
+  })
+})
+
+describe('basic crossword with two words', () => {
+  beforeEach(() => {
+    let crossWord = []
   })
 })

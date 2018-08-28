b4b8476818200a0a931d14a90c26877a1ae7b246
Daniel Miller <danielmiller5791@gmail.com>
2018-08-28 13:08:38 +0100

    refactored and removed superflous function

--- intro/recursion_practice/digit.js
+++ intro/recursion_practice/digit.js
@@ -1,16 +1,25 @@
 class Digit {
 
-  digitSum(n) {
-    console.log(n)
-    if (n < 10) {return n}
-    return this.digitSum(addTheDigits(n))
+  digitSum(n, k) {
+
+    console.log(`n = ${n}, k = ${k}`)
+    n = addTheDigits(n) * k
+    console.log(`n = ${n}`)
+    return calculate(addTheDigits(n))
   }
 
+}
+
+function calculate(int) {
+  console.log(int)
+  if (int < 10) {return int }
+  return calculate(addTheDigits(int))
 }
 
 function addTheDigits( n) {
   return (n.toString().split('').map(x => Number(x)).reduce((a, b) => a + b))
 }
+
 
 module.exports = Digit
 
--- intro/recursion_practice/digit.spec.js
+++ intro/recursion_practice/digit.spec.js
@@ -6,6 +6,13 @@ describe('Recursive super digit calculator', () => {
     digitCalculator = new digit()
   })
   it('aggregates the sum of the integers', () => {
-    expect(digitCalculator.digitSum(9999)).toEqual(9)
+    expect(digitCalculator.digitSum(9999, 1)).toEqual(9)
+  })
+  it('appends the digits to original number k times', () => {
+    expect(digitCalculator.digitSum(1234, 2)).toEqual(2)
+    expect(digitCalculator.digitSum(148, 3)).toEqual(3)
+    expect(digitCalculator.digitSum(123, 3)).toEqual(9)
+    expect(digitCalculator.digitSum(9875, 4)).toEqual(8)
+
   })
 })

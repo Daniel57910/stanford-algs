1fe34e9ddbbaee945d7d6aec462ede23e7813bca
Daniel Miller <danielmiller5791@gmail.com>
2018-08-30 12:11:22 +0100

    refactoring finished, removed repetitive code. merging into master then writing more tests

--- intro/recursion_practice/password.js
+++ intro/recursion_practice/password.js
@@ -11,19 +11,13 @@ class Password {
 
 function solve(passWord, passChecker, attempt, passWordArray, index) {
 
- 
-  if (passWordArray.join('') === passWord) {
-    return passWordArray.join(" ")
-  }
+  if (passWordArray.join('') === passWord) { return passWordArray.join(" ") }
+  
+  if (index === attempt.length) { return "WRONG PASSWORD" }
 
-  if (index === attempt.length) {
-    return "WRONG PASSWORD"
-  }
-
-  if (isAMatch(passChecker, attempt[index]) || attempt[index] === passChecker) {
+  if ( isAMatch(passChecker, attempt[index], subtractDistance(passChecker, attempt[index]), passChecker.length ) || attempt[index] === passChecker) {
     passWordArray.unshift(attempt[index])
-    passChecker = passChecker.slice(0, passChecker.length - attempt[index].length)
-    return solve(passWord, passChecker, attempt, passWordArray, 0)
+    return solve(passWord, sliceWord(passChecker, 0, subtractDistance(passChecker, attempt[index])), attempt, passWordArray, 0)
   }
   else {
     return solve(passWord, passChecker, attempt, passWordArray, index + 1)
@@ -31,12 +25,16 @@ function solve(passWord, passChecker, attempt, passWordArray, index) {
 
 }
 
-function isAMatch(passChecker, currentWord) {
-  return (sliceWord(passChecker, currentWord) === currentWord)
+function subtractDistance(word1, word2) {
+  return word1.length - word2.length
 }
 
-function sliceWord(passChecker, currentWord) {
-  return passChecker.slice(passChecker.length - currentWord.length, passChecker.length)
+function isAMatch(passChecker, currentWord, index1, index2) {
+  return (sliceWord(passChecker, index1, index2) === currentWord)
+}
+
+function sliceWord(passChecker, index1, index2) {
+  return passChecker.slice(index1, index2)
 }
 
 module.exports = Password

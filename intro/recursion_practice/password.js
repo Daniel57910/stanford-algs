1d1b57880fd4e8ecb29cad1b51db32759a033099
Daniel Miller <danielmiller5791@gmail.com>
2018-08-30 09:34:48 +0100

    completed first test of one word password pass or fail

--- intro/recursion_practice/password.js
+++ intro/recursion_practice/password.js
@@ -2,21 +2,46 @@ class Password {
   
   passwordCracker(pass, attempt) {
     attempt = attempt.split(' ')
-    const passWord = pass
     let passChecker = pass
     let passWordArray = []
-    solve(passWord, passChecker, attempt, passWordArray)
+    return solve(pass, passChecker, attempt, passWordArray)
     
   }
 
 }
 
-function solve(passWord, passChecker, attempt, passWordArrray) {
+function solve(passWord, passChecker, attempt, passWordArray) {
+
+  if (passWordArray.join("") === passWord) {
+    console.log(`password Array is correct ${passWordArray}`)
+    return passWordArray.join("")
+  }
 
   for (currentWord of attempt) {
     console.log(`current word is ${currentWord}`)
+
+    if (currentWord === passChecker) {
+      passWordArray.unshift(currentWord)
+      return passWordArray.join("")
+
+    }
+    if (isAMatch(passChecker, currentWord)) {
+      console.log(`match between ${passChecker} + ${currentWord}`)
+      passWordArray.unshift(currentWord)
+      return solve(passWord, sliceWord(passChecker, currentWord), attempt, passWordArrray)
+    }
   }
 
+  return "WRONG PASSWORD"
+
+}
+
+function isAMatch(passChecker, currentWord) {
+  return (sliceWord(passChecker, currentWord) === currentWord)
+}
+
+function sliceWord(passChecker, currentWord) {
+  return passChecker.substring(currentWord.length, passChecker.length)
 }
 
 module.exports = Password
--- intro/recursion_practice/passwordRunner.js
+++ intro/recursion_practice/passwordRunner.js
@@ -1,4 +1,11 @@
+
+let a = "word2word3"
+let b = "word2word3"
+
+console.log(a.substring(b.length, a.length))
+
+
+
 const Password = require('./password')
 let pass = new Password()
-pass.passwordCracker('johnlukemarkhamilton', 'john luke mark hamilton mike')
-
+pass.passwordCracker('johnlukemarkhamilton', 'john luke mark hamilton')

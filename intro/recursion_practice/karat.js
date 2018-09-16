398418a0ccf54ffc29f0d5786e41893cdcf86a9d
Daniel Miller <danielmiller5791@gmail.com>
2018-09-16 16:20:29 +0100

    parsing numbers as strings, need to parse as int

---
+++ intro/recursion_practice/karat.js
@@ -0,0 +1,4 @@
+const karatsuba = require('./karatsuba')
+
+let karats = new karatsuba()
+karats.multiply(1244, 4510)
--- intro/recursion_practice/karatsuba.js
+++ intro/recursion_practice/karatsuba.js
@@ -17,6 +17,15 @@ class Karatsuba {
     if (firstNum < 10 && secondNum < 10) {
       return firstNum * secondNum
     }
+    firstNum = firstNum.toString()
+    secondNum = secondNum.toString()
+    let firstHigh = firstNum.slice(0, firstNum.length / 2)
+    let secondHigh = secondNum.slice(0, secondNum.length / 2)
+    let firstLow = firstNum.slice(firstNum.length / 2, firstNum.length)
+    let secondLow = secondNum.slice(secondNum.length / 2, secondNum.length)
+    console.log(`firstHigh is ${firstHigh}, secondHigh is ${secondHigh}`)
+    console.log(`firstLow is ${firstLow}, secondLow is ${secondLow}`)
+
   
   }
 

ddcc2667a2a70ff796a1b4e4eb344bc902382338
Daniel Miller <danielmiller5791@gmail.com>
2018-10-11 22:07:48 +0100

    refactored and understood role of mid reference point

--- intro/recursion_practice/karatsuba.js
+++ intro/recursion_practice/karatsuba.js
@@ -7,22 +7,21 @@ class Karatsuba {
        return firstNum * secondNum
      }
 
-     var first = firstNum.toString()
-     var second = secondNum.toString()
-     var length = (first.length > second.length ? first.length : second.length)
+     let first = firstNum.toString()
+     let second = secondNum.toString()
+     let length = (first.length > second.length ? first.length : second.length)
+     let mid = length - Math.round(length / 2)
 
-     var subLeng = Math.round(length / 2)
-    
+     let firstHigh = parseInt(first.slice(0, mid))
+     let secondHigh = parseInt(second.slice(0, mid))
+     let firstLow = parseInt(first.slice(mid, first.length))
+     let secondLow = parseInt(second.slice(mid, second.length))
 
-     let firstHigh = parseInt(first.substring(0, first.length - subLeng))
-     let secondHigh = parseInt(second.substring(0, second.length - subLeng))
-     let firstLow = parseInt(first.substring(first.length - subLeng, first.length))
-     let secondLow = parseInt(second.substring(second.length - subLeng, second.length))
 
      let a = this.multiply(firstHigh, secondHigh)
      let b = this.multiply(firstLow, secondLow)
      let c = this.multiply(firstHigh + firstLow, secondHigh + secondLow) - (a + b)
-     return a * Math.pow(10, 2 * subLeng) + c * Math.pow(10, subLeng) + b
+     return a * Math.pow(10, 2 * mid) + c * Math.pow(10, mid) + b
  
   }
 

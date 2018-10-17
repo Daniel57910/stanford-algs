f56f1812a7a12fe93b49feab6021daac02d72c64
Daniel Miller <danielmiller5791@gmail.com>
2018-10-17 08:41:18 +0100

    merge sort implemented and returning 0, need to work out counting of elements

---
+++ intro/inversion_count/inversion.js
@@ -0,0 +1,43 @@
+function inversion_count(array) {
+
+  if (array.length === 1) {
+    return 0
+  }
+
+  let middlePointer = Math.floor(array.length / 2)
+  let leftArray = array.slice(0, middlePointer)
+  let rightArray = array.slice(middlePointer, array.length)
+
+  inversion_count(leftArray)
+  inversion_count(rightArray)
+  count_and_sort(leftArray, rightArray, array)
+
+}
+
+function count_and_sort(leftArray, rightArray, array) {
+   let rightIndex = leftIndex = count = 0;
+   while (rightIndex < rightArray.length && leftIndex < leftArray.length) {
+     
+     if (leftArray[leftIndex] < rightArray[rightIndex]) {
+       array[count] = leftArray[leftIndex]
+       leftIndex++
+     } else {
+       array[count] = rightArray[rightIndex]
+       rightIndex++
+     }
+     count++
+   }
+
+   for (rightIndex; rightIndex < rightArray.length; rightIndex++) {
+     array[count] = rightArray[rightIndex]
+     count++
+   }
+
+   for (leftIndex; leftIndex < leftArray.length; leftIndex++) {
+     array[count] = leftArray[leftIndex]
+     count++
+   }
+
+}
+
+exports.inversion = inversion_count
---
+++ intro/inversion_count/inversion_runner.js
@@ -0,0 +1,6 @@
+const inversion = require('./inversion_2')
+var inversion_count = inversion.inversion
+
+let array = [1, 3, 9, 2, 1, 5, 6]
+inversion_count(array)
+console.log(array)

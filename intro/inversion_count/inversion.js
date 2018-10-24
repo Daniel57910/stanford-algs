b2a9104d8a44bfc648ff2f51f44d3d647cc0417d
Daniel Miller <danielmiller5791@gmail.com>
2018-10-24 08:39:18 +0100

    inversion count working on small test cases, need to test against larger ones

--- intro/inversion_count/inversion.js
+++ intro/inversion_count/inversion.js
@@ -1,9 +1,10 @@
+var inversionCount = 0
+/*try with 0 passed in as arg & returned */
 function inversion_count(array) {
 
   if (array.length === 1) {
-    return 0
+    return
   }
-  let countSwap = 0
 
   let middlePointer = Math.floor(array.length / 2)
   let leftArray = array.slice(0, middlePointer)
@@ -11,44 +12,37 @@ function inversion_count(array) {
 
   inversion_count(leftArray)
   inversion_count(rightArray)
-  console.log(leftArray)
-  console.log(rightArray)
-  countSwap += count_and_sort(leftArray, rightArray, array)
-  console.log(`count swap is ${countSwap}`)
-  console.log(array)
-  return countSwap
-  
+  count_and_sort(leftArray, rightArray, array)
+  return inversionCount
 }
 
 function count_and_sort(leftArray, rightArray, array) {
-   let rightIndex = leftIndex = count = inversionCount = 0
+   let rightIndex = leftIndex = count = 0
+   let length = leftArray.length
 
    while (rightIndex < rightArray.length && leftIndex < leftArray.length) {
      if (leftArray[leftIndex] < rightArray[rightIndex]) {
        array[count] = leftArray[leftIndex]
        leftIndex++
      } else {
-       array[count] = rightArray[rightIndex]
-      inversionCount += 1 + leftIndex - rightIndex
-      console.log(`inversion count is ${inversionCount}`)
+      array[count] = rightArray[rightIndex]
+      inversionCount += (length - leftIndex)
       rightIndex++
      }
      count++
    }
+
+    for (rightIndex; rightIndex < rightArray.length; rightIndex++) {
+      array[count] = rightArray[rightIndex]
+      inversionCount += (length - leftIndex)
+      count++
+    }
 
   for (leftIndex; leftIndex < leftArray.length; leftIndex++) {
     array[count] = leftArray[leftIndex]
     count++
   }
 
-  for (rightIndex; rightIndex < rightArray.length; rightIndex++) {
-    array[count] = rightArray[rightIndex]
-    inversionCount += 1 + leftIndex - rightIndex
-    console.log(`inversion count is ${inversionCount}`)
-    count++
-  }
-
-  return inversionCount
 
 }
 
--- intro/inversion_count/inversion_runner.js
+++ intro/inversion_count/inversion_runner.js
@@ -1,7 +1,7 @@
 const inversion = require('./inversion')
 var inversion_count = inversion.inversion
 
-let array = [2, 1, 3, 1, 2]
+let array = [1, 3, 2]
 let countSwap = inversion_count(array)
-console.log(array)
 console.log(countSwap)
+console.log(array)

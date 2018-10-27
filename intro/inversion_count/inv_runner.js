d8e6027dd96a5e6984a669f25692f7e583eaf4ae
Daniel Miller <danielmiller5791@gmail.com>
2018-10-27 16:17:06 +0100

    learnt how to read file & finished inversion_counter

--- intro/inversion_count/inv_runner.js
+++ intro/inversion_count/inv_runner.js
@@ -1,12 +1,12 @@
 const inversion = require('./inversion')
-const readLine = require('readline')
-const fs = require('fs')
-const readline = require('readline');
 
 var readLineSync = require('read-each-line-sync')
+var inversionCount = inversion.inversion
+
 var array = []
 readLineSync('int_file.txt', 'utf8', function(line) {
   array.push(parseInt(line))
 })
-console.log(array)
-console.log('done')
+
+console.log(inversionCount(array))
+

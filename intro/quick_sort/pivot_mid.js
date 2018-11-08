34065d8b332478a35bb930cd06a8d8ccc8fb956a
Daniel Miller <danielmiller5791@gmail.com>
2018-11-08 08:47:36 +0000

    identifying median in even array, need to test on odd array and incorporate into quicksort

--- intro/quick_sort/pivot_mid.js
+++ intro/quick_sort/pivot_mid.js
@@ -1,70 +1,62 @@
-let arr = [1, 3, 9, 5, 2]
-let even = [1, 0, 3, 4]
+function q_sort(arr) {
 
-function return_median_index(arr) {
-  let candidates = []
+  let right = arr.length
+  let left = 0
+  var partitions_count = 0
 
-  candidates.push(arr[0])
-  candidates.push(arr[find_mid_point(arr)])
-  candidates.push(arr[arr.length - 1])
+  divide(arr, left, right - 1)
+  return partitions_count
 
-  candidates.sort()
-  return candidates[find_mid_point(candidates)]
+  function divide(array, left, right) {
+
+    //base case for returning from function
+    if (right <= left || array.length <= 1) {
+      return
+    }
+
+    /*quicksort the array and retrieve the wall where Ai <= p <= Ai */
+    partition(array, left, right)
+    // partitions_count += right - left
+
+    // divide(array, left, wall - 1)
+    // divide(array, wall + 1, right)
+
+  }
+
 }
 
-function find_mid_point(array) {
-  return Math.floor((array.length - 1) / 2)
+function partition(arr, leftSide, RightSide) {
+
+  let midPoint = find_mid_point(arr)
+  let pivot = return_median_index(arr)
+  console.log(`pivot = ${pivot}`)
+
+  if (pivot === arr[leftSide]) {
+    console.log(`left side is the mid of the three`)
+  }
+  else if (pivot === arr[midPoint]) {
+    console.log(`mid point is mid of three`)
+  }
+  else {
+    console.log(`end point is mid of the three`)
+  }
+  // swap(arr, leftSide, RightSide)
+  // let pivot = arr[leftSide]
+  // let wall = leftSide + 1
+
+  // for (let left = leftSide + 1; left <= RightSide; left++) {
+  //   if (pivot > arr[left]) {
+  //     swap(arr, wall, left)
+  //     wall++
+  //   }
+  // }
+
+  // swap(arr, wall - 1, leftSide)
+
+  // return wall - 1
+  // /* wall acts as partition point for everything around array */
+
 }
-
-console.log(return_median_index(arr))
-console.log(return_median_index(even))
-
-
-// function q_sort(arr) {
-
-//   let right = arr.length
-//   let left = 0
-//   var partitions_count = 0
-
-//   divide(arr, left, right - 1)
-//   return partitions_count
-
-//   function divide(array, left, right) {
-
-//     //base case for returning from function
-//     if (right <= left || array.length <= 1) {
-//       return
-//     }
-
-//     /*quicksort the array and retrieve the wall where Ai <= p <= Ai */
-//     let wall = partition(array, left, right)
-//     partitions_count += right - left
-
-//     divide(array, left, wall - 1)
-//     divide(array, wall + 1, right)
-
-//   }
-
-// }
-
-// function partition(arr, leftSide, RightSide) {
-//   swap(arr, leftSide, RightSide)
-//   let pivot = arr[leftSide]
-//   let wall = leftSide + 1
-
-//   for (let left = leftSide + 1; left <= RightSide; left++) {
-//     if (pivot > arr[left]) {
-//       swap(arr, wall, left)
-//       wall++
-//     }
-//   }
-
-//   swap(arr, wall - 1, leftSide)
-
-//   return wall - 1
-//   /* wall acts as partition point for everything around array */
-
-// }
 
 // function swap(arr, current, wall) {
 //   let temp = arr[current]
@@ -72,4 +64,20 @@ console.log(return_median_index(even))
 //   arr[wall] = temp
 // }
 
-// module.exports = q_sort
+function return_median_index(arr) {
+  let candidates = []
+
+  candidates.push(arr[0])
+  candidates.push(arr[find_mid_point(arr)])
+  candidates.push(arr[arr.length - 1])
+  candidates.sort((a, b) => { return a - b })
+  return candidates[find_mid_point(candidates)]
+}
+
+function find_mid_point(array) {
+  return Math.floor((array.length - 1) / 2)
+}
+
+
+
+module.exports = q_sort
--- intro/quick_sort/q_runner.js
+++ intro/quick_sort/q_runner.js
@@ -1,33 +1,39 @@
 const q_right = require('./pivot_end')
 const q_left = require('./pivot_start')
+const q_mid = require('./pivot_mid')
 var readLineSync = require('read-each-line-sync')
 
-function isSorted(arr) {
+// function isSorted(arr) {
 
-  for (let i = 0; i < arr.length - 1; i++) {
-    if (arr[i] > arr[i + 1]) {
-      return false
-    }
-  }
-  return true
-}
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     if (arr[i] > arr[i + 1]) {
+//       return false
+//     }
+//   }
+//   return true
+// }
 
-var arrRight = []
-var arrLeft = []
+// var arrRight = []
+// var arrLeft = []
 
-readLineSync('q_sort_int.txt', 'utf8', function(line) {
-  arrRight.push(parseInt(line))
-  arrLeft.push(parseInt(line))
-})
-
-
-let leftC = q_left(arrLeft)
-let rightC = q_right(arrRight)
-
-console.log(isSorted(arrRight))
-console.log(isSorted(arrLeft))
-console.log(leftC)
-console.log(rightC)
+// readLineSync('q_sort_int.txt', 'utf8', function(line) {
+//   arrRight.push(parseInt(line))
+//   arrLeft.push(parseInt(line))
+// })
 
 
+// let leftC = q_left(arrLeft)
+// let rightC = q_right(arrRight)
 
+// console.log(isSorted(arrRight))
+// console.log(isSorted(arrLeft))
+// console.log(leftC)
+// console.log(rightC)
+
+
+let arr_end_ev = [1, 3, 9, 5, 2]
+let arr_mid_ev = [0, 3, 6, 7, 12]
+let arr_start_ev = [5, 2, 1, 6, 7, 12]
+q_mid(arr_end_ev)
+q_mid(arr_mid_ev)
+q_mid(arr_start_ev)

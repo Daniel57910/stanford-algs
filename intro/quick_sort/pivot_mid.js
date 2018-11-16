0bb17e00efaefe80cdd4c9ebcf2a88b05ab4390b
Daniel Miller <danielmiller5791@gmail.com>
2018-11-16 21:14:51 +0000

    sorting on middle pivot, need to file sync tomorrow then complete course

--- intro/quick_sort/pivot_mid.js
+++ intro/quick_sort/pivot_mid.js
@@ -15,11 +15,11 @@ function q_sort(arr) {
     }
 
     /*quicksort the array and retrieve the wall where Ai <= p <= Ai */
-    partition(array, left, right)
-    // partitions_count += right - left
+    let wall = partition(array, left, right)
+    partitions_count += right - left
 
-    // divide(array, left, wall - 1)
-    // divide(array, wall + 1, right)
+    divide(array, left, wall - 1)
+    divide(array, wall + 1, right)
 
   }
 
@@ -28,54 +28,50 @@ function q_sort(arr) {
 function partition(arr, leftSide, RightSide) {
 
   let midPoint = find_mid_point(arr)
-  let pivot = return_median_index(arr)
-  console.log(`pivot = ${pivot}`)
+  let pivot = return_median_index(arr[leftSide], arr[midPoint], arr[RightSide])
 
-  if (pivot === arr[leftSide]) {
-    console.log(`left side is the mid of the three`)
+  if (pivot === arr[midPoint]) {
+    swap(arr, leftSide, midPoint)
   }
-  else if (pivot === arr[midPoint]) {
-    console.log(`mid point is mid of three`)
+
+  else if (pivot === arr[RightSide]) {
+    swap(arr, leftSide, RightSide)
   }
-  else {
-    console.log(`end point is mid of the three`)
-  }
-  // swap(arr, leftSide, RightSide)
-  // let pivot = arr[leftSide]
-  // let wall = leftSide + 1
 
-  // for (let left = leftSide + 1; left <= RightSide; left++) {
-  //   if (pivot > arr[left]) {
-  //     swap(arr, wall, left)
-  //     wall++
-  //   }
-  // }
+  let wall = leftSide + 1
 
-  // swap(arr, wall - 1, leftSide)
+   for (let left = leftSide + 1; left <= RightSide; left++) {
+     if (pivot > arr[left]) {
+       swap(arr, wall, left)
+       wall++
+     }
+   }
 
-  // return wall - 1
-  // /* wall acts as partition point for everything around array */
-
+   swap(arr, wall - 1, leftSide)
+   return wall - 1
+  
 }
 
-// function swap(arr, current, wall) {
-//   let temp = arr[current]
-//   arr[current] = arr[wall]
-//   arr[wall] = temp
-// }
+function swap(arr, current, wall) {
+  let temp = arr[current]
+  arr[current] = arr[wall]
+  arr[wall] = temp
+}
 
-function return_median_index(arr) {
+function return_median_index(left, mid, right) {
   let candidates = []
 
-  candidates.push(arr[0])
-  candidates.push(arr[find_mid_point(arr)])
-  candidates.push(arr[arr.length - 1])
+  candidates.push(left)
+  candidates.push(mid)
+  candidates.push(right)
+
   candidates.sort((a, b) => { return a - b })
   return candidates[find_mid_point(candidates)]
 }
 
 function find_mid_point(array) {
   return Math.floor((array.length - 1) / 2)
+
 }
 
 
--- intro/quick_sort/q_runner.js
+++ intro/quick_sort/q_runner.js
@@ -3,15 +3,15 @@ const q_left = require('./pivot_start')
 const q_mid = require('./pivot_mid')
 var readLineSync = require('read-each-line-sync')
 
-// function isSorted(arr) {
+function isSorted(arr) {
 
-//   for (let i = 0; i < arr.length - 1; i++) {
-//     if (arr[i] > arr[i + 1]) {
-//       return false
-//     }
-//   }
-//   return true
-// }
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] > arr[i + 1]) {
+      return false 
+    }
+  }
+  return true
+}
 
 // var arrRight = []
 // var arrLeft = []
@@ -30,10 +30,21 @@ var readLineSync = require('read-each-line-sync')
 // console.log(leftC)
 // console.log(rightC)
 
+big_arr_random = []
+
+for (let i = 0; i < 100000; i++) {
+  big_arr_random.push(Math.floor(Math.random() * 100))
+}
 
 let arr_end_ev = [1, 3, 9, 5, 2]
-let arr_mid_ev = [0, 3, 6, 7, 12]
+let arr_mid_ev = [1, 8, 6, 2, 12]
 let arr_start_ev = [5, 2, 1, 6, 7, 12]
 q_mid(arr_end_ev)
 q_mid(arr_mid_ev)
 q_mid(arr_start_ev)
+q_mid(big_arr_random)
+
+console.log(arr_end_ev)
+console.log(arr_mid_ev)
+console.log(arr_start_ev)
+console.log(isSorted(big_arr_random))

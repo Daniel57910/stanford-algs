606565b578978f6a621d64410fb8816fd5f5e985
Daniel Miller <danielmiller5791@gmail.com>
2018-11-27 20:51:02 +0000

    basic connections matrix completed, need to review with rest of vertices, create object then begin working on file data

--- intro/graph_intro/graph_basics.js
+++ intro/graph_intro/graph_basics.js
@@ -4,20 +4,37 @@ plots the vertices on in a 2d matrix. good for dense graphs as 0(n2 space) for a
 
 var all_nodes = [1, 2, 3, 4, 5, 6, 7, 8]
 var vertices = [[1, 2, 3, 4], [2, 4, 3, 7], [3, 1, 4, 5] [5, 6, 4, 7], [6, 5], [7, 8, 4, 2, 5], [8, 7]]
+
+var test_vertix = [{"node" : 1, "vertex": [2, 3, 4]}, {"node": 2, "vertex": [4, 3, 7]}]
+
 let connections = []
 
-develop_matrix(all_nodes, connections)
+develop_boolean_matrix(all_nodes, connections)
+generate_boolean_graph(test_vertix, connections) 
 console.log(connections)
 
 
-function develop_matrix(all_nodes, connections) {
+function develop_boolean_matrix(all_nodes, connections) {
 
   for (let col_count = 0; col_count < all_nodes.length; col_count++) {
     let current_row = []
     for (let row_count = 0; row_count < all_nodes.length; row_count++) {
-     col_count == 0 || row_count == 0 ? current_row[row_count] = Math.max(row_count, col_count) + 1 : current_row[row_count] = false
+     col_count == 0 || row_count == 0 ? current_row[row_count] = `${Math.max(row_count, col_count) + 1}  ` : current_row[row_count] = false
     }
     connections.push(current_row)
   }
   
 }
+
+function generate_boolean_graph(test_vertix, connections) {
+  for (i of test_vertix) {
+    update_graph(i.node, i.vertex, connections)
+  }
+  //update_graph(key, vertices, connections)
+}
+function update_graph(key, values, connections) {
+  for (i of values) {
+    connections[key][i] = true
+  }
+}
+

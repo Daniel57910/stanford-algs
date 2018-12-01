1923ec5dce197b621983282becd075b4361d1f43
Daniel Miller <danielmiller5791@gmail.com>
2018-12-01 12:11:17 +0000

    removed nodes from graph which have been entirely collapsed, fused into others

--- intro/graph_intro/min_cut_easy.js
+++ intro/graph_intro/min_cut_easy.js
@@ -2,13 +2,11 @@ const Pair = require(`./src/pair`)
 const Graph = require(`./src/graph`)
 
 var graph = new Graph()
-keys = [`A`, `B`, `C`, `D`]
-values = [[`B`, `D`], [`D`, `A`, `C`], [`D`, `B`], [`A`, `B`, `C`]]
+keys = [`A`, `B`, `C`, `D`, 'E']
+values = [[`B`, `D`], [`D`, `A`, `C`], [`D`, `B`], [`A`, `B`, `C`], []]
 
 for (key in keys) {
   graph.add_to_graph(new Pair(keys[key], values[key]))
 }
 
-
-console.log(graph.node_pairs)
 graph.fuse()
--- intro/graph_intro/src/graph.js
+++ intro/graph_intro/src/graph.js
@@ -10,24 +10,38 @@ class Graph {
 
   fuse() {
     this.node_for_fusion = this.node_pairs[identify_for_fusion(this.node_pairs.length)]
-    this.vertex_for_fusion = this.node_for_fusion.vertices[identify_for_fusion(this.node_for_fusion.vertices.length)]
-    this.combine_nodes()
+
+    if (all_vertices_have_been_fused(this.node_for_fusion.vertices)) {
+      this.node_pairs = this.node_pairs.filter((node) => node.node != this.node_for_fusion.node)
+    }
+    else {
+      this.vertex_for_fusion = this.node_for_fusion.vertices[identify_for_fusion(this.node_for_fusion.vertices.length)]
+      this.combine_nodes()
+    }
+    
   }
+
   combine_nodes() {
     this.remove_opposites()
     this.node_for_fusion.node += this.vertex_for_fusion
     this.node_for_fusion.vertices = this.node_for_fusion.vertices.filter((vertex) => vertex != this.vertex_for_fusion)
-    console.log(this.node_pairs)
   }
+
+  /*first remove opposites to prevent self loops and as the graph is undirectional if AD to be fused then DA need to be fused*/
+  
   remove_opposites() {
-    this.opposite_node = this.node_pairs.find((node) => node.node === this.vertex_for_fusion)
-    this.opposite_node.node += this.node_for_fusion.node
-    this.opposite_node.vertices = this.opposite_node.vertices.filter((vertex) => vertex != this.node_for_fusion.node)
+    let opposite_node = this.node_pairs.find((node) => node.node === this.vertex_for_fusion)
+    opposite_node.vertices = opposite_node.vertices.filter((vertex) => vertex != this.node_for_fusion.node)
   }
+
 }
 
 function identify_for_fusion(node_pairs) {
   return Math.floor(Math.random() * node_pairs)
+}
+
+function all_vertices_have_been_fused(vertex_length) {
+  return vertex_length.length === 0
 }
 
 module.exports = Graph

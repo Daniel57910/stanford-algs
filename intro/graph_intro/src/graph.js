a6e2a58c8603ed00cfe928241a4953b847a29677
Daniel Miller <danielmiller5791@gmail.com>
2018-12-11 20:09:36 +0000

    min cut fusing on correct points, need to ensure runs and records number of cuts

--- intro/graph_intro/src/graph.js
+++ intro/graph_intro/src/graph.js
@@ -9,39 +9,45 @@ class Graph {
   }
 
   fuse() {
+    while (this.node_pairs.length > 2) {
+      print_debug(this.node_pairs, "before")
       this.find_the_nodes()
-      this.remove_the_vertex()
       this.combine_the_nodes()
-      print_debug(this.node_pairs)
+      this.remove_the_vertex()
+      this.remove_fused_node()
+      this.update_the_graph()
+      print_debug(this.node_pairs, "after")
+    }
   }
 
   find_the_nodes() {
      this.node_for_fusion = this.node_pairs[identify_for_fusion(this.node_pairs.length)]
      this.vertex_for_fusion = this.node_for_fusion.vertices[identify_for_fusion(this.node_for_fusion.vertices.length)]
-     this.opposite_node_for_fusion = this.node_pairs.find((node) => node.node === this.vertex_for_fusion)
   }
 
   remove_the_vertex() {
-    this.node_for_fusion.vertices = filter_the_vertices(this.node_for_fusion.vertices, this.opposite_node_for_fusion.node)
-    this.opposite_node_for_fusion.vertices = filter_the_vertices(this.opposite_node_for_fusion.vertices, this.node_for_fusion.node)
+    this.node_for_fusion.vertices = [...(new Set(this.node_for_fusion.vertices.map((item) => item)))]
+    this.node_for_fusion.vertices = this.node_for_fusion.vertices.filter((vertex) => !this.node_for_fusion.node.includes(vertex))
   }
 
   combine_the_nodes() {
     this.node_for_fusion.node += this.vertex_for_fusion
-    this.opposite_node_for_fusion.node = this.node_for_fusion.node
-    this.update_the_graph()
+    this.node_for_fusion.vertices =  this.node_for_fusion.vertices.concat(find_pair_for_fusing(this.node_pairs, this.vertex_for_fusion))
+  }
+
+  remove_fused_node() {
+    this.node_pairs = this.node_pairs.filter((node) => node.node != this.vertex_for_fusion)
   }
 
   update_the_graph() {
-    for (let i = 0; i < this.node_pairs.length; i++)
-      if (this.node_pairs[i].node != this.node_for_fusion.node) {
-        this.node_pairs[i].vertices = update_additional_vertices(this.node_pairs[i].vertices, this.node_for_fusion.node)
-      }
+    for (let i in this.node_pairs) {
+      this.node_pairs[i].vertices = update_additional_vertices(this.node_pairs[i].vertices, this.node_for_fusion.node)
     }
-  }
+  } 
+}
 
 function update_additional_vertices(vertex_array, fused_node) {
-  for (let i = 0; i < vertex_array.length; i++) {
+  for (let i in vertex_array) {
     if (fused_node.includes(vertex_array[i])) {
       vertex_array[i] = fused_node
     }
@@ -49,20 +55,18 @@ function update_additional_vertices(vertex_array, fused_node) {
   return [...(new Set(vertex_array.map((item) => item)))]
 }
 
-function print_debug(node_array) {
+function print_debug(node_array, status) {
+  console.log(status)
   node_array.map((node) => console.log(`node = ${node.node} => ${node.vertices}`))
-}
-
-function filter_the_vertices(vertex_array, node){
-  return vertex_array.filter((vertex) => vertex != node)
 }
 
 function identify_for_fusion(node_pairs) {
   return Math.floor(Math.random() * node_pairs)
 }
 
-function all_vertices_have_been_fused(vertex_length) {
-  return vertex_length.length === 0
-}
+function find_pair_for_fusing(node_array, vertex) {
+  let node = node_array.find((node) => node.node === vertex)
+  return node.vertices
+} 
 
 module.exports = Graph
